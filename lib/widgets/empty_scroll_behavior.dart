fe02a00459008cf41f52a21516ae1a477c0c9fa5
cheshiren <cheshire@ifiction.ru>
2020-03-09 18:07:14 +0500

    Отключил эффект оверскролла (см. https://www.instagram.com/p/B9bOP8OAlUq/)

---
+++ lib/widgets/empty_scroll_behavior.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Поведение скролла, обнуляющее эффект оверскролла (см. https://www.instagram.com/p/B9bOP8OAlUq/)
+
+class EmptyScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+    BuildContext context,
+    Widget child,
+    AxisDirection axisDirection,
+  ) {
+    return child;
+  }
+}
--- lib/widgets/log_view.dart
+++ lib/widgets/log_view.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widgets/flutter_widgets.dart';
 
 import '../init.dart';
+import '../widgets/empty_scroll_behavior.dart';
 
 class LogView extends StatefulWidget {
   Function scrollTFC;
   LogView(this.scrollTFC);
-  
+
   @override
   _LogViewState createState() => _LogViewState();
 }
@@ -14,24 +15,28 @@ class LogView extends StatefulWidget {
 class _LogViewState extends State<LogView> {
   @override
   Widget build(BuildContext context) {
-    return ScrollablePositionedList.builder(
-      itemScrollController: itemScrollController,
-      // itemPositionsListener: itemPositionListener,
-      itemCount: log.length,
-      // initialScrollIndex: log.length-1,
-      itemBuilder: (ctx, index) {
-        return RichText(
-            textAlign: log[index].style == currentThemeData.textTheme.headline6
-                ? TextAlign.center
-                : TextAlign.left,
-            text: TextSpan(children: <TextSpan>[
-              log[index],
-              TextSpan(
-                style: currentThemeData.textTheme.bodyText2,
-                text: '\n',
-              ),
-            ]));
-      },
+    return ScrollConfiguration( // виджет для отключения эффекта оверскролла (см. https://www.instagram.com/p/B9bOP8OAlUq/)
+      behavior: EmptyScrollBehavior(),
+      child: ScrollablePositionedList.builder(
+        itemScrollController: itemScrollController,
+        // itemPositionsListener: itemPositionListener,
+        itemCount: log.length,
+        // initialScrollIndex: log.length-1,
+        itemBuilder: (ctx, index) {
+          return RichText(
+              textAlign:
+                  log[index].style == currentThemeData.textTheme.headline6
+                      ? TextAlign.center
+                      : TextAlign.left,
+              text: TextSpan(children: <TextSpan>[
+                log[index],
+                TextSpan(
+                  style: currentThemeData.textTheme.bodyText2,
+                  text: '\n',
+                ),
+              ]));
+        },
+      ),
     );
   }
 
